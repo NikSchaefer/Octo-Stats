4037f29b42571906e46493f554b39343a2feef24
Nik Schaefer <nikkschaefer@gmail.com>
2021-01-01 20:15:04 -0600

    Sorting + More

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,14 @@ import * as React from "react"
 import Axios from 'axios'
 import { userData, defUserData, Repo } from './interfaces'
 import './index.css'
-import { IconFork, IconRepo, IconStar } from "./svg"
+import * as Icon from "./svg"
 function Box(props: { num: number, name: string }) {
+  function numberWithCommas(x: number) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
   return (
     <div className='box'>
-      <h2>{props.num}</h2>
+      <h2>{numberWithCommas(props.num)}</h2>
       <h3>{props.name}</h3>
     </div>
   )
@@ -18,20 +21,34 @@ function Graph(props: {}) {
     </div>
   )
 }
-function Repos(props: { arr: Repo[] }): any {
+function Repos(props: { arr: Repo[], sort: sort }): any {
   let out = []
-  const arr: Repo[] = props.arr.sort(function (a, b) {
-    return b.stargazers_count - a.stargazers_count
-  })
+  let arr: Repo[] = []
+  if (props.sort === 'size') {
+    arr = props.arr.sort(function (a, b) {
+      return b.size - a.size
+    })
+  }
+  else if (props.sort === 'forks') {
+    arr = props.arr.sort(function (a, b) {
+      return b.forks_count - a.forks_count
+    })
+  }
+  else {
+    arr = props.arr.sort(function (a, b) {
+      return b.stargazers_count - a.stargazers_count
+    })
+
+  }
   for (let i = 0; i < props.arr.length; i++) {
     out.push(
       <a href={arr[i].html_url} className="repo">
-        <h2><IconRepo /> {arr[i].name}</h2>
+        <h2><Icon.Repo /> {arr[i].name}</h2>
         <p>{arr[i].description}</p>
         <div className="repo__bottom">
-          <IconStar />
+          <Icon.Star />
           <p>{arr[i].stargazers_count}</p>
-          <IconFork />
+          <Icon.Fork />
           <p>{arr[i].forks_count}</p>
           <p style={{ fontSize: '15px', marginLeft: 'auto' }}>{arr[i].size} KB</p>
         </div>
@@ -40,15 +57,18 @@ function Repos(props: { arr: Repo[] }): any {
   }
   return out
 }
+
+type sort = "stars" | "forks" | 'size'
 export default function IndexPage() {
 
-  const [user, setUser] = React.useState<string>('NikSchaefer')
+  const [user, setUser] = React.useState<string>('eddiejaoude')
   const [userData, setUserData] = React.useState<userData>(defUserData)
 
   const [repoData, setRepoData] = React.useState<Repo[]>([])
+  const [sortType, setSortType] = React.useState<sort>("stars")
 
   async function GetRepo() {
-    const data = await Axios.get(`https://api.github.com/users/${user}/repos`)
+    const data = await Axios.get(`https://api.github.com/users/${user}/repos?per_page=10000`)
     setRepoData(data.data)
   }
   async function Get() {
@@ -67,11 +87,14 @@ export default function IndexPage() {
           <h1>{userData.name}</h1>
           <a href={userData.html_url}>@{user}</a>
           <p>{userData.bio}</p>
+          <div className="info__subtext">
+            <Icon.Location /><p>{userData.location}</p>
+          </div>
         </div>
         <div className="boxes">
-          <Box num={222} name='Repositories' />
-          <Box num={222} name='Repositories' />
-          <Box num={222} name='Repositories' />
+          <Box num={userData.public_repos} name='Repositories' />
+          <Box num={userData.followers} name='Followers' />
+          <Box num={userData.following} name='Following' />
         </div>
       </section>
       <div id='graph__color' />
@@ -82,7 +105,16 @@ export default function IndexPage() {
       </section>
 
       <section className="repos">
-        <Repos arr={repoData} />
+        <div className="sorting-options">
+          <h1 className='repos__title'>Top Repos</h1>
+          <div className='sort-by'>
+            <h2>Sort By</h2>
+            <p onClick={function () { setSortType('stars') }}>Stars</p>
+            <p onClick={function () { setSortType('forks') }}>Forks</p>
+            <p onClick={function () { setSortType('size') }}>Size</p>
+          </div>
+        </div>
+        <Repos sort={sortType} arr={repoData} />
       </section>
 
     </main>
